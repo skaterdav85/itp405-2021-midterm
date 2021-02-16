eecf4604577c12c685de7305b8868b73b0aa3759
David Tang <dtang859@gmail.com>
2021-02-15 23:30:14 -0800

    Fix issue where the route function is generating URLs with http instead of https on Heroku

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,13 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\QuestionController;
 use App\Http\Controllers\AnswerController;
+use Illuminate\Support\Facades\URL;
 
 Route::get('/', [QuestionController::class, 'index'])->name('questions.index');
 Route::get('/questions/{id}', [QuestionController::class, 'show'])->name('questions.show');
 Route::post('/questions', [QuestionController::class, 'store'])->name('questions.store');
 Route::post('/answers', [AnswerController::class, 'store'])->name('answers.store');
+
+if (env('APP_ENV') !== 'local') {
+    URL::forceScheme('https');
+}
